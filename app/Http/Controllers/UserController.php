b30d840d1d0cc5634bd26753804f132d7ba2f79a
pvillor <pvillor@gmail.com>
2023-09-04 20:34:04 -0300

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateUserRequest;
+use App\Services\CreateUserService;
+
+class UserController extends Controller
+{
+    public function create(CreateUserRequest $request)
+    {
+        // return User::create($request->all());
+        $createUserService = new CreateUserService();
+
+        return $createUserService->execute($request->all());
+    }
+}
